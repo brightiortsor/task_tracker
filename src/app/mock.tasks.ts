345286778eb0b4e0ff72ff4e4e5f647c22cf69ff
Bright Iortsor <iortsor@gmail.com>
2023-03-02 12:21:38 +0100

    added mock json data

---
+++ src/app/mock.tasks.ts
@@ -0,0 +1,40 @@
+import { Task } from "./Types";
+
+export const TASKS:Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: 'Gym',
+    day: 'May 8th at 11:30am',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Vacation shopping',
+    day: 'May 9th at 10:30am',
+    reminder: false,
+  },
+  {
+    id: 6,
+    text: 'Dinner with friends',
+    day: 'May 10th at 9:30am',
+    reminder: true,
+  },
+];
