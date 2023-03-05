b810b06ad9eb8c5c75e90a89a99105abac282a26
Bright Iortsor <iortsor@gmail.com>
2023-03-05 09:16:12 +0100

    added hide/show toggle function for addTask

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -10,10 +10,10 @@ import { Task } from '../../Types';
 })
 export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
-  text: string;
-  day: string;
+  text!: string;
+  day!: string;
   reminder: boolean = false;
-  showAddTask: boolean;
+  showAddTask!: boolean;
   subscription: Subscription;
 
   constructor(private uiService: UiService) {
